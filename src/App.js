892a59e2016af9c4638b1cbdcb316e31d81a8c54
Michael Dinnall <michaeldinnall@gmail.com>
2019-10-31 11:30:13 -0400

    added finishing css and fixed display

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 const Buttons = ({ values, divName, handleButtonClick }) => {
   return (
     <div className={divName}>
-      {values.map(element => <button value={element} onClick={handleButtonClick}>{element}</button>)}
+      {values.map(element => 
+        <button 
+        key={element.toString()}
+        value={element}
+        onClick={handleButtonClick}>
+        {element}
+        </button>)}
     </div>
   )
 }
@@ -65,12 +71,141 @@ checkLength = () => {
     }
   }
 
+  checkKeyForExpression = () => {
+    const { expression, value, clickedValue } = this.state
+
+    let newExpression
+
+    // When user clicks on equals sign 
+    if (clickedValue === '=' || clickedValue === 'Enter') {
+      if (value || value === 0) {
+        this.setState({
+          clearOrDelete: 'CLR',
+          expression: value.toString(),
+          value: '',
+          clickedValue: ''
+        })
+      }
+
+      // When user clicks on DEL sign 
+    } else if (clickedValue === 'DEL' || clickedValue === 'Backspace') {
+      // Slice the last element of the expression and assign to newExpression 
+      newExpression = expression.slice(0, expression.length - 1)
+
+      // And if this new expression is blank  
+      if (!newExpression) {
+        this.setState({
+          expression: newExpression,
+          value: '',
+          clickedValue: ''
+        })
+
+        // If the last element of the new expression is a number 
+      } else if (!isNaN(newExpression[newExpression.length - 1])) {
+        // Evaluate 
+        let newValue = this.evaluate(newExpression)
+        this.setState({
+          expression: newExpression,
+          value: newValue,
+          clickedValue: ''
+        })
+      } else {
+        // Keep the value 
+        this.setState({
+          expression: newExpression,
+          clickedValue: ''
+        })
+      }
+
+      // When user clicks on CLR sign 
+    } else if (clickedValue === 'CLR' || clickedValue === 'c') {
+      // Switch the CLR button to DEL button and clear the screen 
+      this.setState({
+        clearOrDelete: 'DEL',
+        expression: '',
+        value: '',
+        clickedValue: ''
+      })
+
+      // When the expression's length is less than 17, go ahead with pressing numbers and operators 
+    } else if (expression.length < 17) {
+
+      // When user clicks on any number 
+      if (!isNaN(clickedValue)) {
+        let lastValueOfExp = expression[expression.length - 1]
+
+        // If there's no expression yet or if the last value of expression is an operator 
+        if (!expression || this.operators.includes(lastValueOfExp)) {
+
+          // Don't allow user to type 0 
+          if (clickedValue !== '0') {
+            newExpression = expression + clickedValue
+            let newValue = this.evaluate(newExpression)
+
+            this.setState({
+              clearOrDelete: 'DEL',
+              expression: newExpression,
+              value: newValue,
+              clickedValue: ''
+            })
+          }
+
+          // Otherwise allow all numbers and operators 
+        } else {
+          newExpression = expression + clickedValue
+          let newValue = this.evaluate(newExpression)
+
+          this.setState({
+            clearOrDelete: 'DEL',
+            expression: newExpression,
+            value: newValue,
+            clickedValue: ''
+          })
+
+        }
+
+        // When user clicks on anything other than the 3 types listed above 
+      } else {
+        let lastValueOfExp = expression[expression.length - 1]
+
+        // If clicked value is an operator 
+        if (this.operators.includes(clickedValue)) {
+
+          // Check to see that the expression is filled and last value of expression is NOT an operator 
+          if (expression && !this.operators.includes(lastValueOfExp)) {
+            newExpression = expression + clickedValue
+            this.setState({
+              expression: newExpression,
+              clickedValue: '',
+              keyPress: true
+            }, () => {
+              setTimeout(() => {
+                this.setState({
+                  keyPress: false
+                })
+              }, 300)
+            })
+          }
+
+        } else {
+          // Otherwise, add to the string 
+          newExpression = expression + clickedValue
+
+          this.setState({
+            expression: newExpression,
+            clickedValue: ''
+          })
+        }
+      }
+    }
+ }
 
   handleKey = e => {
     let clickedValue = e.key
     if (this.allowedKeys.includes(clickedValue)) {
       this.setState({ clickedValue }, () => {
         // check expression of each press
+        this.checkKeyForExpression()
       })
     }
   }
@@ -81,31 +216,42 @@ checkLength = () => {
 
     this.setState({ clickedValue }, () => {
       // check expression of each press
+      this.checkKeyForExpression()
     })
   }
 
 
 render(){
-      const { clearOrDelete, expression, value, clickedValue } = this.state
+      const { clearOrDelete, expression, value } = this.state
+      console.log("EXpression:",expression)
+          // Add class to expression div depending on the length of expression 
+      const expressionClass = this.checkLength()
+
      return (
         <div className='calculator-container' >
-         <form className='calculator'>
+          <form className='calculator'>
+         <h2 className='intro-title'>REACT CALCULATOR</h2>
+          <p className='intro-title'>Do some Math.</p>
+            <div className={`expression ${expressionClass}`}>
+              <p>{expression}</p>
+            </div>
+            <div className='value'>
+              <p>{value}</p>
+            </div>
             <div className='buttons-container'>
-             <Buttons
-              values={this.inner}
-              divName='numbers'
-              handleButtonClick={this.handleButtonClick} />
-            <div className='operations'>
-              <button value={clearOrDelete} onClick={this.handleButtonClick} >{clearOrDelete}</button>
-              <button value='/' onClick={this.handleButtonClick}>÷</button>
-              <button value='*' onClick={this.handleButtonClick}>x</button>
-              <button value='+' onClick={this.handleButtonClick}>+</button>
-              <button value='-' onClick={this.handleButtonClick}>-</button>
+              <Buttons
+                values={this.inner}
+                divName='numbers'
+                handleButtonClick={this.handleButtonClick} />
+              <div className='operations'>
+                <button value={clearOrDelete} onClick={this.handleButtonClick} >{clearOrDelete}</button>
+                <button value='/' onClick={this.handleButtonClick}>÷</button>
+                <button value='*' onClick={this.handleButtonClick}>x</button>
+                <button value='+' onClick={this.handleButtonClick}>+</button>
+                <button value='-' onClick={this.handleButtonClick}>-</button>
+              </div>
             </div>
-            </div>
-
-
-         </form>
+          </form>
         </div>
        )
    }
